56786482a8492e7bccde98087cec8b2001ab2a30
Robert O'Brien <robrien@localhost.localdomain>
2020-02-11 23:31:25 +0000

    Added required libraries

--- app/src/main/java/com/obrienrobert/kubely/MainActivity.kt
+++ app/src/main/java/com/obrienrobert/kubely/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.obrienrobert.kubely
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.zsmb.materialdrawerkt.builders.drawer
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        drawer {
+
+        }
+
     }
 }
