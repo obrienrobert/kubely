564ce0575a889205e09c17febac6daa3bf7043c4
Robert O'Brien <robrien@localhost.localdomain>
2020-02-13 14:48:57 +0000

    Added navigation drawer and fragments

---
+++ app/src/main/java/com/obrienrobert/fragments/PodFragment.kt
@@ -0,0 +1,35 @@
+package com.obrienrobert.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.obrienrobert.kubely.R
+
+class PodFragment : Fragment() {
+
+    lateinit var textView: TextView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.pod_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView = view.findViewById(R.id.pod_fragment_text)
+        textView.text  = "Pod Fragment"
+
+    }
+
+    companion object {
+        fun newInstance(): PodFragment {
+            return PodFragment()
+        }
+    }
+}
--- app/src/main/java/com/obrienrobert/kubely/MainActivity.kt
+++ app/src/main/java/com/obrienrobert/kubely/MainActivity.kt
@@ -1,21 +1,58 @@
 package com.obrienrobert.kubely
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import co.zsmb.materialdrawerkt.builders.drawer
+import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
+import co.zsmb.materialdrawerkt.draweritems.divider
+import com.obrienrobert.fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 
 class MainActivity : AppCompatActivity(), AnkoLogger{
+
+    private val fragmentManager = supportFragmentManager
+    private val homeFragment = HomeFragment()
+    private val podFragment = PodFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        drawer {
 
+        /* Display First Fragment initially */
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.homeFragment, homeFragment)
+        fragmentTransaction.commit()
+
+        drawer {
+            primaryItem("Home") {
+                onClick { _ ->
+                    Log.d("DRAWER", "Home")
+                    navigateTo(HomeFragment.newInstance())
+                    false
+                }
+            }
+            divider {}
+            primaryItem("Pods") {
+                onClick { _ ->
+                    Log.d("DRAWER", "Pods")
+                    navigateTo(PodFragment.newInstance())
+                    false
+                }
+
+            }
         }
 
+    }
+
+    private fun navigateTo(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.homeFragment, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
